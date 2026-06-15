aea6726e172b510069a5ade3a9cf1f5e4f8b0fd7
kyzchosen1x <kyzchosen1x@gmail.com>
2026-06-15 12:46:07 +0000

    Sync LeetCode submission Runtime - 1 ms (17.59%), Memory - 43.1 MB (89.40%)

---
+++ 1505-create-target-array-in-the-given-order/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        ArrayList<Integer> target = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            target.add(index[i], nums[i]);
+        }
+
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = target.get(i);
+        }
+
+        return result;
+    }
+}
