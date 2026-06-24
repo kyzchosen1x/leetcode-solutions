f259c55db85c2ae6737a85af48959ab41d665846
kyzchosen1x <kyzchosen1x@gmail.com>
2026-06-24 11:41:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.3 MB (16.17%)

---
+++ 0374-guess-number-higher-or-lower/solution.java
@@ -0,0 +1,22 @@
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int start = 1;
+        int end = n;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            int result = guess(mid);
+
+            if (result == 0) {
+                return mid;
+            } else if (result == 1) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
