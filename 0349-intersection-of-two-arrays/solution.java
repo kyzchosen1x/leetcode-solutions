a51cc4887075c014e955e482cacf21e8ab35f026
kyzchosen1x <kyzchosen1x@gmail.com>
2026-06-28 16:11:15 +0000

    Sync LeetCode submission Runtime - 8 ms (8.46%), Memory - 44.8 MB (81.91%)

---
+++ 0349-intersection-of-two-arrays/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    if (!result.contains(nums2[j])) {
+                        result.add(nums2[j]);
+                    }
+                }
+            }
+        }
+
+        int[] ans = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            ans[i] = result.get(i);
+        }
+
+        return ans;
+    }
+}
